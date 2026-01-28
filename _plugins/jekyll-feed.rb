737b58b8e216f3e8da6c338e4610198d8f329135
ubbyboi-tech <EMAIL_USED_FOR_UBBYBOI_TECH>
2026-01-28 18:33:55 +0300

    jekyll-feed

--- _plugins/jekyll-feed.rb
+++ _plugins/jekyll-feed.rb
@@ -1,4 +1,9 @@
 Jekyll::Hooks.register :site, :after_init do |site|
-    site.config['feed'] ||= {}
-    site.config['feed']['excerpt_only'] = false
-  end
+  site.config['feed'] ||= {}
+
+  # Use full content in the generated feed
+  site.config['feed']['excerpt_only'] = false
+
+  # Move jekyll-feed output away from /feed.xml so our custom feed.xml is served
+  site.config['feed']['path'] = 'rss.xml'
+end
